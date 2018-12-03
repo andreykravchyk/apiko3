794b5f0281203eb9e344a0b32253755fd496ab53
andreykravchyk_ <www.kravchyk.ua@gmail.com>
2018-12-04 01:33:09 +0200

    Initial commit

---
+++ build/precache-manifest.798bf9e03f699022e4b03228c2a97df5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d12fad7930bb18168968",
+    "url": "/React-To-Do/static/css/main.6b1844f5.chunk.css"
+  },
+  {
+    "revision": "d12fad7930bb18168968",
+    "url": "/React-To-Do/static/js/main.d12fad79.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/React-To-Do/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "377f3f2a0d1ee2473f68",
+    "url": "/React-To-Do/static/js/runtime~main.377f3f2a.js"
+  },
+  {
+    "revision": "cc8bd3cba81315dee84d7c0f72496a6c",
+    "url": "/React-To-Do/index.html"
+  }
+];
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+export default class Header extends React.Component{
+	render(){
+		return(
+		<h1>Книги які Ви хочете проситати</h1>
+		);
+	}
+}
